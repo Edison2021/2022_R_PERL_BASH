95958357d17bd7813ae1b4a6c643d0e9bc5fd6cd
Edison2021 <43031197+Edison2021@users.noreply.github.com>
2022-03-17 12:46:49 +1300

    Update phylosignal.R

--- phylosignal.R
+++ phylosignal.R
@@ -1,11 +1,13 @@
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly=TRUE)
 library(phylosignal)
 library(adephylo)
 library(ape)
 library(phylobase)
-p4d<-read.p4d("rooted.nwk","trait.txt",phylo.format="newick",data.format="table") 
-barplot.phylo4d(p4d, tree.type = "phylo", tree.ladderize = TRUE)
-barplot(p4d, tree.type = "fan", tip.cex = 0.6, tree.open.angle = 160, trait.cex = 0.6)
-barplot(p4d, trait.bg.col = c("#F6CED8", "#CED8F6", "#CEF6CE"), bar.col = "grey35")
+p4d<-read.p4d(args[1],args[2],phylo.format="newick",data.format="table") 
+#barplot.phylo4d(p4d, tree.type = "phylo", tree.ladderize = TRUE)
+#barplot(p4d, tree.type = "fan", tip.cex = 0.6, tree.open.angle = 160, trait.cex = 0.6)
+#barplot(p4d, trait.bg.col = c("#F6CED8", "#CED8F6", "#CEF6CE"), bar.col = "grey35")
 barplot(p4d, center= FALSE, trait.bg.col = c("#F6CED8", "#CED8F6", "#CEF6CE"), bar.col = "grey35")
 phyloSignal(p4d = p4d, method = "all")
 crlg <- phyloCorrelogram(p4d, trait = "glu")
@@ -19,4 +21,3 @@ S1 2 3 4
 S2 5 6 7
 S3 8 9 01
 note: only continuous traits data allowed.
-
