878f8455a8e578f451f93cef3731bbc49a0a0758
Edison2021 <43031197+Edison2021@users.noreply.github.com>
2022-03-17 17:58:46 +1300

    Update tree_APheatmap.R

--- tree_APheatmap.R
+++ tree_APheatmap.R
@@ -1,6 +1,7 @@
 #Plots a presence absence heatmap for a given set of genes given a tree
 #usage Rscript tree_APheatmap.R -t N13.nwk  -g total.1.matrix.txt.tr.txt -s gene.list -o test.pdf
 # adjust the ggtree::vexpand to control margin 
+# use csvtk convert format
 library(argparse)
 library(ggtree)
 library(ggplot2)
