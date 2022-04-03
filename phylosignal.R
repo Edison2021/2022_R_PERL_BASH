c5237769f66780db4ffcd37fcd37be91a9871079
Edison2021 <43031197+Edison2021@users.noreply.github.com>
2022-04-04 11:19:04 +1200

    Update phylosignal.R

--- phylosignal.R
+++ phylosignal.R
@@ -22,6 +22,7 @@ plot(crlg)
 #S3 8 9 01
 #note: only continuous traits data allowed.
 #K < 1, the evolution of traits is independent of the phylogeny, and when K > 1 they are dependent of the phylogeny, any large value possible
+#This statistic can indicate little or no phylogenetic signal (K < < 1)
 #λ = 0 indicating no phylogenetic signal, 1 indicating a strong phylogenetic signal, value falls bwtween 0~1
 #I equals 0, species resemble each other as much as predicted under a brownian motion model. <0, less than predicted under a brownian motion model.
 #> 0, closely related species are more similar. Cmean simlar to I value
