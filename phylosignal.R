feeaefdd490600a2bd338c232d8d3f4cc1cd20bd
Edison2021 <43031197+Edison2021@users.noreply.github.com>
2022-06-05 13:49:30 +1200

    Update phylosignal.R

--- phylosignal.R
+++ phylosignal.R
@@ -10,7 +10,7 @@ p4d<-read.p4d(args[1],args[2],phylo.format="newick",data.format="table")
 #barplot.phylo4d(p4d, tree.type = "phylo", tree.ladderize = TRUE)
 #barplot(p4d, tree.type = "fan", tip.cex = 0.6, tree.open.angle = 160, trait.cex = 0.6)
 #barplot(p4d, trait.bg.col = c("#F6CED8", "#CED8F6", "#CEF6CE"), bar.col = "grey35")
-# this will plot the traits
+# this will plot the traits; default is center=TRUE and scale=TRUE
 #If center is TRUE then centering is done by subtracting the column means (omitting NAs) of x from their corresponding columns.
 #If scale is TRUE then scaling is done by dividing the (centered) columns of x by their standard deviations if center is TRUE, and the root mean square otherwise. 
 #barplot(p4d, center= FALSE, trait.bg.col = c("#F6CED8", "#CED8F6", "#CEF6CE","#FFE5CC"), bar.col = "grey35")
